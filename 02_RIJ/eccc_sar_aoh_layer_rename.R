8f33d1d978e276662df41c9467ac5ac246a0b97a
Dan Wismer <daniel.s.wismer@gmail.com>
2025-05-07 10:54:51 -0600

    update CRS name attribute to Canada_Albers_WGS_1984
    all projection params are good

--- 02_RIJ/eccc_sar_aoh_layer_rename.R
+++ 02_RIJ/eccc_sar_aoh_layer_rename.R
@@ -3,10 +3,15 @@ eccc_sar_aoh_path <- "C:/Data/NAT/SPECIES_1km/AOH_SAR_Layers_1KM"
 eccc_sar_aoh_RENAME_path <- "C:/Data/NAT/SPECIES_1km/AOH_SAR_Layers_1KM_rename"
 eccc_sar_aoh <- list.files(eccc_sar_aoh_path, pattern = ".tif$", full.names = TRUE)
 
+NCC_CRS <- terra::rast("C:/Data/PRZ/NAT_DATA/NAT_1KM_20240729/_1km/Idx.tif") |>
+  terra::crs()
+
 for (i in seq_along(eccc_sar_aoh)) {
+  browser()
   r <- rast(eccc_sar_aoh[i])
   base_with_extention <- basename(eccc_sar_aoh[i])
   base <- tools::file_path_sans_ext(basename(eccc_sar_aoh[i]))
   names(r) <- base
-  writeRaster(r, file.path(eccc_sar_aoh_RENAME_path,base_with_extention), overwrite = TRUE)
+  crs(r) <- NCC_CRS # update CRS name from unknown to Canada_Albers_WGS_1984.
+  writeRaster(r, file.path(eccc_sar_aoh_RENAME_path, base_with_extention), overwrite = TRUE)
 }
